697bb40f6f69038f52673fcb1a668d26ae5cc1ba
longhui520 <39285837+longhui520@users.noreply.github.com>
2018-07-06 09:12:14 +0800

    万网

--- 实战项目/万维网期刊/js/register.js
+++ 实战项目/万维网期刊/js/register.js
@@ -3,26 +3,93 @@ $(function(){
     let phoneNumber=$('.phone-number>div>input')
     let userName=$('.user-name>div>input')
     let userPsd=$('.user-psd>div>input')
-    let userPsdAgain=$('.user-psd-agian>div>input')
+    let userPsdAgain=$('.user-psd-again>div>input')
     let verification=$('.verification>div>input')
+    // console.log(userPsd)
     phoneNumber.on({
         'focus':function(){
             $(this).val('') 
         },
         'blur':function(){
             if(checkMobile($(this).val())){
-                $(this).parent().addClass('ok')
-                $(this).parent().next().css('visibility','hidden')
+                $(this).parent().addClass('ok').next().css('visibility','hidden')
             }else{
-                $(this).parent().removeClass('ok')
-                $(this).parent().next().css('visibility','visible')
+                $(this).css('borderColor','red').parent().removeClass('ok').next().css('visibility','visible')
             }
-          
-        }
+        }    
+    })
+    userName.on({
+        'focus':function(){
+            $(this).val('') 
+        },
+        'blur':function(){
+            if(checkName($(this).val())){
+                $(this).parent().addClass('ok').next().css('visibility','hidden')
+            }else{
+                $(this).css('borderColor','red').parent().removeClass('ok').next().css('visibility','visible')
+            }
+        }    
+    })
+    userPsd.on({
+        'focus':function(){
+            $(this).val('') 
+        },
+        'blur':function(){
+            if(CheckPassWord($(this).val())){
+                $(this).parent().addClass('ok').next().css('visibility','hidden')
+            }else{
+                $(this).css('borderColor','red').parent().removeClass('ok').next().css('visibility','visible')
+            }
+        }    
+    })
+    userPsdAgain.on({
+        'focus':function(){
+            $(this).val('') 
+        },
+        'blur':function(){
+            if($(this).val()===userPsd.val()){
+                $(this).parent().addClass('ok').next().css('visibility','hidden')
+            }else{
+                $(this).css('borderColor','red').parent().removeClass('ok').next().css('visibility','visible')
+            }
+        }    
+    })
+    verification.on({
+        'focus':function(){
+            $(this).val('') 
+        },
+        'blur':function(){
+            if($(this).val()==='1234'){
+                $(this).parent().addClass('ok').next().css('visibility','hidden')
+            }else{
+                $(this).css('borderColor','red').parent().removeClass('ok').next().css('visibility','visible')
+            }
+        }    
     })
     function checkMobile(value){ 
         if((/^1[3|4|5|7|8][0-9]{9}$/.test(value))){ 
          return true
         } 
-    } 
+    }
+    function checkName(value){
+        if(/^[a-zA-Z\d]\w{3,11}[a-zA-Z\d]$/.test(value)){
+            return true
+        }
+    }
+    function CheckPassWord(password) {//必须为字母加数字且长度不小于8位
+        var str = password;
+         if (str == null || str.length <8) {
+             return false;
+         }
+         var reg1 = new RegExp(/^[0-9A-Za-z]+$/);
+         if (!reg1.test(str)) {
+             return false;
+         }
+         var reg = new RegExp(/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/);
+         if (reg.test(str)) {
+             return true;
+         } else {
+             return false;
+         }
+     }
 })
