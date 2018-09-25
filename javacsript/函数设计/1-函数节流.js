51f483297f86ee77e11ba32b3291327c5c32f9c5
longhui520 <39285837+longhui520@users.noreply.github.com>
2018-09-25 21:42:09 +0800

    add in 2018.9.25

---
+++ javacsript/函数设计/1-函数节流.js
@@ -0,0 +1,18 @@
+//函数节流是让某些代码可以在间断的情况下，连续重复执行
+var processor={
+    timeId:null,
+    performProcessing:function(){
+        //业务代码
+    },
+    process:function(){
+        clearTimeout(this.timeId);
+        var that=this
+        this.timeId=setTimeout(function(){that.performProcessing();},100)
+    }
+}
+processor.process()
+//简写
+function throttle(method,context){
+    clearTimeout(method.id);
+    method.id=setTimeout(function(){method.call(context)})
+}.
